090825739398b7ea8e1765c5c496980d78aa4781
tranan <tranquocan1508@gmail.com>
2024-10-04 20:47:23 +0700

    feat: pond repository

--- KoiCareSys.Data/Repository/PondRepository.cs
+++ KoiCareSys.Data/Repository/PondRepository.cs
@@ -2,9 +2,11 @@
 using KoiCareSys.Data.DAO;
 using KoiCareSys.Data.Models;
 using KoiCareSys.Data.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,23 @@ namespace KoiCareSys.Data.Repository
         public PondRepository() 
         {
             _dao ??= new PondDAO();
+        }
+
+        public async Task<Pond> GetPond(Guid id)
+        {
+            return await GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Pond>> GetPonds()
+        {
+            return await GetAllAsync();
+        }
+
+        public async Task<IEnumerable<Pond>> GetAllPond(string search)
+        {
+            Expression<Func<Pond, bool>> predicate = x => x.PondName.Contains(search) || x.Description.Contains(search);
+            IQueryable<Pond> query = _dbSet.Where(predicate);
+            return query.AsNoTracking().AsEnumerable();
         }
     }
 }
