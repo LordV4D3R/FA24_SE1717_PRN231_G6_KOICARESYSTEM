2f64915bbb39b33aa740bd5743162eaf22e96624
iamdwn <truongdinhdongduong@gmail.com>
2024-10-15 01:03:16 +0700

    feat: add CRUD Pond

--- WebApi/KoiCareSys.WebAPI/Controllers/PondController.cs
+++ WebApi/KoiCareSys.WebAPI/Controllers/PondController.cs
@@ -1,5 +1,6 @@
-﻿using KoiCareSys.Service.Service.Interface;
-using Microsoft.AspNetCore.Http;
+﻿using KoiCareSys.Data.DTO;
+using KoiCareSys.Serivice.Base;
+using KoiCareSys.Service.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KoiCareSys.WebAPI.Controllers
@@ -8,11 +9,43 @@ namespace KoiCareSys.WebAPI.Controllers
     [ApiController]
     public class PondController : ControllerBase
     {
-        private readonly IPondService pondService;
+        private readonly IPondService _pondService;
 
-        public PondController()
+        public PondController(IPondService pondService)
         {
+            _pondService = pondService;
         }
 
+        [HttpGet]
+        public async Task<IBusinessResult> GetAll()
+        {
+            return await _pondService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IBusinessResult> GetById(Guid id)
+        {
+            return await _pondService.GetById(id);
+        }
+
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        public async Task<IBusinessResult> Update(PondDTO request)
+        {
+            return await _pondService.Update(request);
+        }
+
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IBusinessResult> Create([FromBody] PondDTO request)
+        {
+            return await _pondService.Create(request);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IBusinessResult> Delete(Guid id)
+        {
+            return await _pondService.DeleteById(id);
+        }
     }
 }
