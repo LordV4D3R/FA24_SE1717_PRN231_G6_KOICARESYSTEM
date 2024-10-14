7cb0dc4832762416515eb6446687bcda22279d5f
Vader <tranquocan1508@gmail.com>
2024-10-14 18:53:05 +0700

    feat: Add UI Razor Page

---
+++ WebAppRazorPage/KoiCareSys.WebApp/ApiService/ApiService.cs
@@ -0,0 +1,30 @@
+﻿using KoiCareSys.WebApp.ApiService.Interface;
+
+namespace KoiCareSys.WebApp.ApiService
+{
+    public class ApiService : IApiService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ApiService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            var client = _clientFactory.CreateClient("MyAPI");
+            var response = await client.GetAsync(endpoint);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+
+        public async Task<T> PostAsync<T>(string endpoint, object data)
+        {
+            var client = _clientFactory.CreateClient("MyAPI");
+            var response = await client.PostAsJsonAsync(endpoint, data);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+    }
+}
