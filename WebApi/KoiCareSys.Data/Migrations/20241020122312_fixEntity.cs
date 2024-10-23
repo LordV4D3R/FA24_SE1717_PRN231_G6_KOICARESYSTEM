7ef2938573e8c1d441dd0978385054d281e71c7c
Vader <tranquocan1508@gmail.com>
2024-10-23 11:18:20 +0700

    refactor: fix entiy

---
+++ WebApi/KoiCareSys.Data/Migrations/20241020122312_fixEntity.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KoiCareSys.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_feeding_schedule_koi_koi_id",
+                schema: "koicare",
+                table: "feeding_schedule");
+
+            migrationBuilder.RenameColumn(
+                name: "koi_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                newName: "pond_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_feeding_schedule_koi_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                newName: "IX_feeding_schedule_pond_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_feeding_schedule_pond_pond_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                column: "pond_id",
+                principalSchema: "koicare",
+                principalTable: "pond",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_feeding_schedule_pond_pond_id",
+                schema: "koicare",
+                table: "feeding_schedule");
+
+            migrationBuilder.RenameColumn(
+                name: "pond_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                newName: "koi_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_feeding_schedule_pond_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                newName: "IX_feeding_schedule_koi_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_feeding_schedule_koi_koi_id",
+                schema: "koicare",
+                table: "feeding_schedule",
+                column: "koi_id",
+                principalSchema: "koicare",
+                principalTable: "koi",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
