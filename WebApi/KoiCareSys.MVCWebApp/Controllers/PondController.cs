808088434c3e49219ab7be2d221dfaf10e18be64
iamdwn <truongdinhdongduong@gmail.com>
2024-10-25 10:14:43 +0700

    fix: adjust search with 3 fields

--- WebApi/KoiCareSys.MVCWebApp/Controllers/PondController.cs
+++ WebApi/KoiCareSys.MVCWebApp/Controllers/PondController.cs
@@ -3,7 +3,6 @@ using KoiCareSys.MVCWebApp.Base;
 using KoiCareSys.MVCWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Newtonsoft.Json;
 
 namespace KoiCareSys.MVCWebApp.Controllers
 {
@@ -22,10 +21,10 @@ namespace KoiCareSys.MVCWebApp.Controllers
             var ponds = new List<PondDto>();
             try
             {
-                var result = await _apiService.GetAsync<BusinessResult>("api/ponds");
-                if (result != null && result.Status == 1)
+                var result = await _apiService.GetAsync<List<PondDto>>("api/ponds");
+                if (result != null)
                 {
-                    ponds = JsonConvert.DeserializeObject<List<PondDto>>(result.Data.ToString());
+                    ponds = result;
                 }
             }
             catch (Exception ex)
@@ -38,8 +37,11 @@ namespace KoiCareSys.MVCWebApp.Controllers
                             (string.IsNullOrEmpty(note) || p.Note.ToLower().Contains(note.ToLower())) &&
                             (string.IsNullOrEmpty(description) || p.Description.ToLower().Contains(description.ToLower())))
                 .ToList();
+
             return View("Index", filteredPonds);
         }
+
+
 
         public async Task<IActionResult> Index()
         {
